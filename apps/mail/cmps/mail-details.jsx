e1840628123a7cc43654ca792bcf3d003781ac18
Tal <tale1000@gmail.com>
2022-08-25 19:28:15 +0300

    add button back in cmp mail-details

--- apps/mail/cmps/mail-details.jsx
+++ apps/mail/cmps/mail-details.jsx
@@ -1,5 +1,18 @@
 export class MailDetails extends React.Component {
-    render ()  {
-        return <h1>hello</h1>
+
+    render() {
+        console.log(this.props)
+        const { subject, body, from, to } = this.props.email
+
+        return <section className="mail-details flex column">
+            <button className="btn-back" onClick={() => this.props.onResetMail()}>⬅</button>
+            <h1 className="subject">{subject}</h1>
+            <div className="from flex">
+                <span className="from-fullName">{from.fullName}</span>
+                <span className="from-email">{`<${from.email}>`}</span>
+            </div>
+            <span className="to-fullName">To: {to.fullName}</span>
+            <p className="body">{body}</p>
+        </section>
     }
 }
