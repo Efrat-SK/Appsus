7c657618b928648560d482a31dc57a4ee496f267
Tal <tale1000@gmail.com>
2022-08-26 14:04:33 +0300

    add humburger in mobile only  to app-header cmp

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,19 +1,41 @@
 const { Link, NavLink } = ReactRouterDOM
 
-export function AppHeader() {
+export class AppHeader extends React.Component {
 
-    return <header className="app-header main-layout full">
-        <div className="header-container flex space-between">
-            <Link to="/">
-                <h3>AppSus</h3>
-            </Link>
-            <nav>
-                <NavLink to="/mail">Mail</NavLink>
-                <NavLink to="/note">Note</NavLink>
-                <NavLink to="/book">Book</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink exact to="/">Home</NavLink>
-            </nav>
-        </div>
-    </header>
+    state = {
+        headerClassName: ''
+    }
+
+    openMenu = () => {
+        this.setState({ headerClassName: 'menu-opened' })
+    }
+
+    closeMenu = () => {
+        this.setState({ headerClassName: '' })
+    }
+
+    render() {
+        const { headerClassName } = this.state
+
+        return <header className={`app-header main-layout full ${headerClassName}`}>
+            <div className="header-container flex space-between">
+                <Link to="/">
+                    <h3>AppSus</h3>
+                </Link>
+                <nav className="flex">
+                    <button className="btn-close" onClick={this.closeMenu}>X</button>
+                    <NavLink to="/mail" onClick={this.closeMenu}>Mail</NavLink>
+                    <span className="separate-line">_____</span>
+                    <NavLink to="/note" onClick={this.closeMenu}>Note</NavLink>
+                    <span className="separate-line">_____</span>
+                    <NavLink to="/book" onClick={this.closeMenu}>Book</NavLink>
+                    <span className="separate-line">_____</span>
+                    <NavLink to="/about" onClick={this.closeMenu}>About</NavLink>
+                    <span className="separate-line">_____</span>
+                    <NavLink exact to="/" onClick={this.closeMenu}>Home</NavLink>
+                </nav>
+                <button className="toggle-menu" onClick={this.openMenu}>☰</button>
+            </div>
+        </header>
+    }
 }
