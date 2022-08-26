23d691ef4aa2c3e910dc347e194a431584ab4e78
Tal <tale1000@gmail.com>
2022-08-26 12:37:07 +0300

    update css fonts in views home and about

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,17 +1,19 @@
-const { Link, NavLink, withRouter } = ReactRouterDOM
+const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
-    return <header className="app-header flex space-between full">
-        <Link to="/">
-            <h3>AppSus</h3>
-        </Link>
-        <nav>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-            <NavLink to="/book">Book</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink exact to="/">Home</NavLink>
-        </nav>
+    return <header className="app-header main-layout full">
+        <div className="header-container flex space-between">
+            <Link to="/">
+                <h3>AppSus</h3>
+            </Link>
+            <nav>
+                <NavLink to="/mail">Mail</NavLink>
+                <NavLink to="/note">Note</NavLink>
+                <NavLink to="/book">Book</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink exact to="/">Home</NavLink>
+            </nav>
+        </div>
     </header>
 }
